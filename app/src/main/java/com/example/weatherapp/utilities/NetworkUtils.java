dbb2747a786ed841ba1da093780d4633f8ac7730
Antor <antor145648s@gmail.com>
2022-01-01 22:35:56 +0600

    Complete Networking

--- app/src/main/java/com/example/weatherapp/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/weatherapp/utilities/NetworkUtils.java
@@ -12,12 +12,20 @@ import java.util.Scanner;
 
 public class NetworkUtils {
 
+    /**
+     * Query parameter
+     */
+    final static String QUERY_PRAM = "?";
+    final static String LAT_PRAM = "lat";
+    final static String LOT_PRAM = "lon";
+    final static String APP_ID_PRAM = "appid";
+    final static String UNITS_PRAM = "units";
+    final static String EXCLUDE_PRAM = "exclude";
+    final static String FORMAT_PRAM = "format";
     private static final String TAG = "NetworkUtils";
-
     //api base url
     private static final String WEATHER_FORECAST_BASE_URL =
             "https://api.openweathermap.org/data/2.5/onecall?";
-
     // values for api call
     private static final String FORMAT = "json";
     private static final String APP_ID = "a242f3acd8adac5febdb6ee2df3bb784";
@@ -25,22 +33,12 @@ public class NetworkUtils {
     private static final String EXCLUDE = "minutely";
 
     /**
-     * Query parameter
-     */
-    final static String QUERY_PRAM = "?";
-    final static String LAT_PRAM = "lat";
-    final static String LOT_PRAM = "lot";
-    final static String APP_ID_PRAM = "appid";
-    final static String UNITS_PRAM = "units";
-    final static String EXCLUDE_PRAM = "exclude";
-    final static String FORMAT_PRAM = "format";
-
-    /**
      * build api query url
+     *
      * @param location gives lat at index 0 and lot at index 1...
      * @return query url
      */
-    public static URL buildUrl(Double[] location){
+    public static URL buildUrl(double[] location) {
         Uri buildUri = Uri.parse(WEATHER_FORECAST_BASE_URL).buildUpon()
                 .appendQueryParameter(LAT_PRAM, location[0] + "")
                 .appendQueryParameter(LOT_PRAM, location[1] + "")
@@ -62,23 +60,28 @@ public class NetworkUtils {
     }
 
     /**
-     *This method return entire result from HTTP response
+     * This method return entire result from HTTP response
      *
      * @param url the url to fetch url response
      * @return HTTP response in a string
      * @throws IOException Related to network and inputStream
      */
-    public static String getResponseFromHttpUrl(URL url)throws IOException{
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
-        try{
+        try {
             InputStream in = urlConnection.getInputStream();
-            Scanner scanner = new Scanner(in);
 
-            if (scanner.hasNext())
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
                 return scanner.next();
-            return null;
-        }finally {
+            } else {
+                return null;
+            }
+        } finally {
             urlConnection.disconnect();
         }
     }
